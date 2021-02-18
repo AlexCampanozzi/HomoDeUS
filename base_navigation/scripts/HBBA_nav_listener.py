1975d79905328b385315f6b1b7d04a45122a4ca8
Charles <chamai@live.ca>
2021-02-18 23:07:05 +0000

    Added a prototype motivation file

--- base_navigation/scripts/HBBA_nav_listener.py
+++ base_navigation/scripts/HBBA_nav_listener.py
@@ -2,16 +2,19 @@
 import rospy
 
 from navigator import Navigator
+from std_msgs.msg import Bool
 from geometry_msgs.msg import PoseStamped
 
 class HBBA_nav_listener(Navigator):
     def __init__(self):
         Navigator.__init__(self)
+        self.result_pub = rospy.Publisher("nav_result", Bool, queue_size=5)
         # navigator inits a node, therefore cant init one here
         # rospy.init_node('HBBA_nav_listener', anonymous=True)
 
     def callback(self, data):
-        self.goto(data.pose.position.x, data.pose.position.y, data.pose.orientation.w)
+        nav_result = self.goto(data.pose.position.x, data.pose.position.y, data.pose.orientation.w)
+        self.result_pub.publish(nav_result)
 
     def listen(self):
         rospy.Subscriber('/hbba_nav_goal', PoseStamped, self.callback)
