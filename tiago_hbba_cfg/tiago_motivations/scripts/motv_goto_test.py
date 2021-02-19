ae96b22921189a66b721e5cdb744547116875d27
Charles <chamai@live.ca>
2021-02-19 18:47:48 -0500

    Fixed a typo, added DesireSet management manually. Still not working tho

--- tiago_hbba_cfg/tiago_motivations/scripts/motv_goto_test.py
+++ tiago_hbba_cfg/tiago_motivations/scripts/motv_goto_test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-# A motivation for environment-based suggestions (close the door, etc.)
+# A simple motivation module to add a GoTo desire
 
 import rospy
 import actionlib
-from hbba_msgs.msg import Desire
+from hbba_msgs.msg import Desire, DesiresSet
 from hbba_msgs.srv import AddDesires, RemoveDesires
 
 class GotoManager:
@@ -12,6 +12,9 @@ class GotoManager:
     def __init__(self):
         self.add_desires    = rospy.ServiceProxy('add_desires', AddDesires)
         self.rem_desires    = rospy.ServiceProxy('remove_desires', RemoveDesires)
+        self.add_desires.wait_for_service()
+        while(not self.add_desires.wait_for_service(rospy.Duration.from_sec(5.0))):
+            rospy.loginfo("Waiting for the hbba desire server to come up")
 
     def add(self):
         des = Desire()
@@ -19,18 +22,24 @@ class GotoManager:
         des.type        = "GoTo"
         des.utility     = 1.0
         des.intensity   = 1.0
-        des.params      = "{fram_id: map, x: -1, y: -1, t: 0}"
+        des.params      = "{frame_id: map, x: -1, y: -1, t: 0}"
 
-        self.add_desires.call([des])
+        desSet = DesiresSet()
+        desSet.desires = [des, Desire()]
+
+        self.add_desires.call(desSet)
 
     def remove(self):
         self.rem_desires.call(["test_goto"])
 
 if __name__ == "__main__":
-    rospy.init_node("motv_goto_test")
+    try:
+        rospy.init_node("motv_goto_test")
 
-    node = GotoManager()
-    node.add()
+        node = GotoManager()
+        node.add()
 
-    rospy.spin()
+        rospy.spin()
 
+    except rospy.ROSInterruptException:
+        pass
