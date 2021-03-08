5b14f421151973e03f6fe0d5ced52e90a25f5173
williamaris <william.aris@usherbrooke.ca>
2021-03-08 15:13:49 -0500

    Cleaned main

--- waiter_state_machine/main.py
+++ waiter_state_machine/main.py
@@ -9,38 +9,40 @@ from behaviors import *
 def main():
     # Creating the node
     rospy.init_node('waiter_state_machine')
+
+    # Initializing the behaviors
     initialiseBehaviors()
 
     # Creating the machine
-    bertrand = StateMachine()
-    print("StateMachineCreated")
+    state_machine = StateMachine()
 
     # Assembling the state machine
-    bertrand.add_state(State00())
-    bertrand.add_state(State01())
-    bertrand.add_state(State02())
-    bertrand.add_state(State03())
-    bertrand.add_state(State04())
-    bertrand.add_state(State05())
-    bertrand.add_state(State06())
-    bertrand.add_state(State07())
-    bertrand.add_state(State08())
-    bertrand.add_state(State09())
-    bertrand.add_state(State10())
-    bertrand.add_state(State11())
-    bertrand.add_state(State12())
+    state_machine.add_state(State00())
+    state_machine.add_state(State01())
+    state_machine.add_state(State02())
+    state_machine.add_state(State03())
+    state_machine.add_state(State04())
+    state_machine.add_state(State05())
+    state_machine.add_state(State06())
+    state_machine.add_state(State07())
+    state_machine.add_state(State08())
+    state_machine.add_state(State09())
+    state_machine.add_state(State10())
+    state_machine.add_state(State11())
+    state_machine.add_state(State12())
 
     # Printing the current state id
-    rospy.loginfo("Switched to state: " + bertrand.get_current_state().get_id())
+    current_state_id = state_machine.get_current_state().get_id()
+    rospy.loginfo("[Main] Switched to state: " + current_state_id)
 
     # Main loop
     while not rospy.is_shutdown():
-        bertrand.run_current_state()
-        
-        if bertrand.check_transitions():
-            rospy.loginfo("Switched to state: " + bertrand.get_current_state().get_id())
-        
-        #rospy.sleep(0.1)
+        state_machine.run_current_state()
+
+        if state_machine.check_transitions():
+            current_state_id = state_machine.get_current_state().get_id()
+            rospy.loginfo("[Main] Switched to state: " + current_state_id)
+
 
 if __name__ == "__main__":
     main()
