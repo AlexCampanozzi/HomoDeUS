15bcf079693a4f263ae72f6f2ddb4402a0de69af
williamaris <william.aris@usherbrooke.ca>
2021-03-08 15:22:23 -0500

    Cleaned menu

--- waiter_state_machine/menu.py
+++ waiter_state_machine/menu.py
@@ -1,11 +1,28 @@
 import re
 
-menu = ["apple", "apples", "banana", "bananas", "orange", "oranges"]
+# List of available items on the fake menu
+menu = [
+    "apple",
+    "apples",
+    "banana",
+    "bananas",
+    "orange",
+    "oranges"]
+
 
 def check_if_any_word_in_menu(sentence):
-    
-    # Remove punctuation and capital letters
-    string = re.sub(r'[^\w\s]','',sentence).lower()
+    """
+    Checks if a sentence contains any word in the menu. The
+    function returns True if "sentence" contains things on the
+    menu.
+
+    Arguments
+    ---------
+        sentence : string
+            A string to verify
+    """
+    # Removing punctuation and capital letters
+    string = re.sub(r'[^\w\s]', '', sentence).lower()
 
     # Checking if there's an intersection
     co_occuring_words = set(menu) & set(string.split())
@@ -14,9 +31,18 @@ def check_if_any_word_in_menu(sentence):
 
 
 def extract_order(sentence):
+    """
+    Find words from the menu that are present in the sentence. The
+    function returns a list containing everything from the menu that
+    is present in the sentence.
 
-    # Remove punctuation and capital letters
-    string = re.sub(r'[^\w\s]','',sentence).lower()
+    Arguments
+    ---------
+        sentence : string
+            A string to check
+    """
+    # Removing punctuation and capital letters
+    string = re.sub(r'[^\w\s]', '', sentence).lower()
 
     # Returning the intersection
     return list(set(menu) & set(string.split()))
