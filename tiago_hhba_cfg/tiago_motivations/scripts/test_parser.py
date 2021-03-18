8a0addd627110c9f878d7608abe00ea6c959ede7
Kitty-Kat <stello4526@hotmail.com>
2021-03-18 16:08:09 -0400

    Added functions for xml comparaison and xml reading

--- tiago_hhba_cfg/tiago_motivations/scripts/test_parser.py
+++ tiago_hhba_cfg/tiago_motivations/scripts/test_parser.py
@@ -1,11 +1,19 @@
 #! /usr/bin/env python
 
 import xml.etree.ElementTree as ET
+import os
     
 #parse the xml containing the contexts
-speech_context_xml_path = 'speech_context.xml'
+speech_context_xml_path = "src/HomoDeUS/tiago_hhba_cfg/tiago_motivations/others/speech_context.xml"
 speech_context = ET.parse(speech_context_xml_path).getroot()
 for element in speech_context.findall('*'):
     print(element.tag)
     for answer in element.findall('answer'):
         print('     ' + answer.text)
+
+
+# print ('step2')
+# teststring = " i am named bob"
+# if 'bob' in teststring:
+#     print('bobfound')
+# print('bobend')
