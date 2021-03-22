eb974631031dac9be9e1aedd2608932da1341782
AlexCampanozzi <steel4444@hotmail.com>
2021-03-22 18:42:26 +0000

    changed the desire classes (now two distinct classes) for the face detection and face tracking

--- face_detection/src/homodeus_proc_face_detection.cpp
+++ face_detection/src/homodeus_proc_face_detection.cpp
@@ -13,8 +13,7 @@ FaceDetector::FaceDetector(ros::NodeHandle& nh):
   _nh(nh)
 {
   // Image topics (Uncomment the appropriate one)
-  //std::string imageTopic = "/usb_cam/image_raw"; // For testing on the laptop
-  std::string imageTopic = "/proc_input_camera_feed"; // For testing on the laptop
+  std::string imageTopic = "/usb_cam/image_raw"; // For testing on the laptop
   //std::string imageTopic = "/xtion/rgb/image_raw"; // For testing on the robot
 
   image_transport::ImageTransport imageTransport(nh);
--- head_control/scripts/homodeus_motv_gaze_following.py
+++ head_control/scripts/homodeus_motv_gaze_following.py
@@ -7,7 +7,7 @@ import actionlib
 from hbba_msgs.msg import Desire, DesiresSet, Event
 from hbba_msgs.srv import AddDesires, RemoveDesires
 
-class GotoManager:
+class GazeFollowingManager:
 
     def __init__(self):
         self.add_desires    = rospy.ServiceProxy('add_desires', AddDesires)
@@ -15,20 +15,29 @@ class GotoManager:
         rospy.wait_for_service("add_desires")
 
     def add(self):
-        des = Desire()
-        des.id          = "test_goto"
-        des.type        = "GoTo"
-        des.utility     = 1.0
-        des.intensity   = 1.0
+        faceDetectionDesire = Desire()
+        faceDetectionDesire.id          = "test_face_detection"
+        faceDetectionDesire.type        = "face_detection"
+        faceDetectionDesire.utility     = 1.0
+        faceDetectionDesire.intensity   = 1.0
+
+        moveHeadDesire = Desire()
+        moveHeadDesire.id          = "test_move_head"
+        moveHeadDesire.type        = "move_head"
+        moveHeadDesire.utility     = 1.0
+        moveHeadDesire.intensity   = 1.0
         #des.params      = "{frame_id: '/xtion_rgb_optical_frame', x: 30.0, y: 0.0, t: 0.0}"
 
-        self.add_desires.call([des])
+        self.add_desires.call([faceDetectionDesire, moveHeadDesire])
 
     def remove(self):
-        self.rem_desires.call(["test_goto"])
+        self.rem_desires.call(["test_face_detection"])
+        self.rem_desires.call(["test_move_head"])
 
     def removeOnEvent(self, event):
-        if event.desire_type == "GoTo" and event.type == Event.ACC_ON:
+        if event.desire_type == "face_detection" and event.type == Event.ACC_ON:
+            self.rem_desires.call([event.desire])
+        elif event.desire_type == "move_head" and event.type == Event.ACC_ON:
             self.rem_desires.call([event.desire])
         else:
             pass
@@ -41,7 +50,7 @@ if __name__ == "__main__":
     try:
         rospy.init_node("tiago_motv_gaze_following", anonymous=False)
 
-        node = GotoManager()
+        node = GazeFollowingManager()
         node.add()
 
         node.observe()
