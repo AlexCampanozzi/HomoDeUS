b714ac53ef811a8f8ae905790611727adebea174
Kitty-Kat <stello4526@hotmail.com>
2021-03-27 14:11:50 -0400

    python packages managed
    setup.py is now working as intended

--- homodeus_behaviors/scripts/bhvr_nav_listener.py
+++ homodeus_behaviors/scripts/bhvr_nav_listener.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 import rospy
 
-from scripts.navigator import Navigator
+from base_navigation.scripts.navigator import Navigator
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import String
 from base_navigation.msg import GoToResult
--- homodeus_external/setup.py
+++ homodeus_external/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 
 d = generate_distutils_setup(
-    packages=['scripts'],
-    package_dir={'': 'base_navigation'}
+    packages=["base_control","base_navigation","head_control","face_detection","keyword_detector"],
+    package_dir={'': ''}
 )
 setup(**d)
