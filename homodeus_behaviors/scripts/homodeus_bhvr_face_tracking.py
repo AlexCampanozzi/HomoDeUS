b85e5cfde278272129d3128942c5a5f970ae6318
Nicolas GB <guen3301@usherbrooke.ca>
2021-04-05 10:13:45 -0500

    Moved PID to common library

--- homodeus_behaviors/scripts/homodeus_bhvr_face_tracking.py
+++ homodeus_behaviors/scripts/homodeus_bhvr_face_tracking.py
@@ -12,7 +12,7 @@ import numpy as np
 from custom_msgs.msg import FacePosition
 from custom_msgs.msg import FacePositions
 
-from head_control.scripts.Utility import PID
+import HomoDeUS_common_py.HomoDeUS_common_py as common  
 
 class FaceTracking:
     def __init__(self):
@@ -32,8 +32,8 @@ class FaceTracking:
 
         self.threshold = 1
 
-        self.pid_x = PID(self.img_center_x, K_P=0.003, K_I=0.0, K_D=0.0)
-        self.pid_y = PID(self.img_center_y, K_P=0.003, K_I=0.0, K_D=0.0)
+        self.pid_x = common.PID(self.img_center_x, K_P=0.003, K_I=0.0, K_D=0.0)
+        self.pid_y = common.PID(self.img_center_y, K_P=0.003, K_I=0.0, K_D=0.0)
 
     def _head_callback(self, detections):
         rospy.loginfo("received face")
