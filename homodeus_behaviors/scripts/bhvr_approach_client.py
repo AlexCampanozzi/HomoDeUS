f60731e762c3ac880ecbcd43cb942dcc84b3ac34
Charles <chamai@live.ca>
2021-04-07 20:44:58 -0400

    Added groundwork for using scanner in approach, addep deps to package files

--- homodeus_behaviors/scripts/bhvr_approach_client.py
+++ homodeus_behaviors/scripts/bhvr_approach_client.py
@@ -2,6 +2,7 @@
 import rospy
 from geometry_msgs.msg import Twist
 from custom_msgs.msg import FacePositions, FacePosition
+from sensor_msgs.msg import LaserScan
 import HomoDeUS_common_py as common
 
 class ClientApproach(self):
@@ -24,6 +25,13 @@ class ClientApproach(self):
 
         self.img_center_x = self.img_width // 2
         self.img_center_y = self.img_height // 2
+
+        self.scan_listener = rospy.Subscriber("bhvr_input_scan", LaserScan, self.scanCB, queue_size=5)
+
+        # To make getting scan params a one-shot
+        self.got_scan_params = False
+
+        self.too_close = False
 
     def approach(self):
         self.boxes_listener = rospy.Subscriber('bhvr_input_faces', FacePositions, self.facesCB)
@@ -66,7 +74,40 @@ class ClientApproach(self):
             command = Twist()
             # invert sign so we move in right direction 
             command.linear.x = -1*size_diff * self.dif_to_vel_factor
+            # or use self.min_range as a distance straight up, maybe?
+            
+            # if too close to stuff back up so rotation or goto can get us around obstacle
+            if command.linear.x > 0 and self.too_close:
+                command.linear.x = -0.01
+
             self.vel_publisher.publish(command)
 
             # Coding this assuming head_tracking will rotate base to center Client: only sending linear commands
 
+    def scanCB(self, scan):
+        scan = LaserScan()
+        if self.got_scan_params == False:
+            self.start_angle = scan.angle_min
+            self.stop_angle = scan.angle_max
+            self.angle_increment = scan.angle_increment
+            self.angle_range = self.stop_angle - self.start_angle
+            self.got_scan_params = True
+        else:
+            # commented because min seems stuck at 0.05 for some reason. Probably ctaching a bit of itself
+            # if scan.range_min < 0.5:
+            #     # too close to stuff
+            #     self.too_close = True
+            # else:
+            #     self.too_close = False
+
+            # Take the ranges only from the 3rd in front to decide what to do
+            half_third = 0.33 / 2
+            points = len(scan.ranges)
+            midway_point = round(points/2)
+            self.max_range = 0
+            self.min_range = 25 #The max range as start value since we look for lower 
+            for i in range(midway_point - half_third, midway_point + half_third):
+                self.max_range = scan.ranges[i] if scan.ranges[i] > self.max_range
+                self.min_range = scan.ranges[i] if scan.ranges[i] < self.min_range
+
+            # TODO: some algo to decide what is a person vs not
