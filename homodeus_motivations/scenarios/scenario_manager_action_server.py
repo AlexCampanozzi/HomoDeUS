4804d2508410ed244f8198fadd9063d3595d11b5
Switch-cm <46759849+Switch-cm@users.noreply.github.com>
2021-04-10 20:22:26 -0400

    added tester to run scenario without selector

--- homodeus_motivations/scenarios/scenario_manager_action_server.py
+++ homodeus_motivations/scenarios/scenario_manager_action_server.py
@@ -11,10 +11,7 @@ class ScenarioManagerAction(object):
     _feedback = custom_msgs.msg._scenario_managerFeedback # Feedback indicating current state when transitions happen
     _result = custom_msgs.msg._scenario_managerResult # Result given when execution terminates
 
-    custom_msgs.msg.scenario_managerAction
-
     def __init__(self, name):
         self._action_name = name
         self._as = actionlib.SimpleActionServer(self._action_name, custom_msgs.msg.scenario_managerAction, execute_cb=self.execute_cb, auto_start = False)
-        print("komdkomngdf komngdkmp nd ")
         self._as.start()
