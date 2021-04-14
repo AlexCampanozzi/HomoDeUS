92988cd83e2e4cac46b2273e85d8f81b910ae3ec
AlexCampanozzi <steel4444@hotmail.com>
2021-04-14 14:53:42 +0000

    tested and fixed the faceTracking

--- homodeus_behaviors/scripts/bhvr_face_tracking.py
+++ homodeus_behaviors/scripts/bhvr_face_tracking.py
@@ -29,21 +29,21 @@ class FaceTracking:
         self.img_height = camera_info.height
         self.img_width = camera_info.width
         self.pub = rospy.Publisher('tiago_head_controller', PoseStamped, queue_size=5)
-        self.pubObserver = rospy.Publisher('Face_tracking_observer', Bool, queue_size=5)
+        self.pubObserver = rospy.Publisher('/Face_tracking_observer', Bool, queue_size=5)
 
         self.img_center_x = self.img_width // 2
         self.img_center_y = self.img_height // 2
 
-        self.threshold = 1
+        self.threshold = 55
 
-        self.pid_x = common.PID(self.img_center_x, K_P=0.003, K_I=0.0, K_D=0.0)
-        self.pid_y = common.PID(self.img_center_y, K_P=0.003, K_I=0.0, K_D=0.0)
+        self.pid_x = common.PID(self.img_center_x, K_P=-0.0010, K_I=-0.0001, K_D=-0.00005)
+        self.pid_y = common.PID(self.img_center_y, K_P=-0.0010, K_I=-0.0001, K_D=-0.00005)
 
     def _head_callback(self, detections):
-        rospy.loginfo("received face")
         main_face_x = 0
         main_face_y = 0
         main_face_dist = 1000000
+        boolMsg = Bool()
 
         # Find the closest face to the image center (main face)
         for face in detections.faces:
@@ -57,11 +57,12 @@ class FaceTracking:
 
         # If the main face is inside the limit, don't move the head
         if main_face_dist < self.threshold:
-            self.pubObserver.publish(True)
-            rospy.loginfo("face centered")
+            boolMsg.data = True
+            self.pubObserver.publish(boolMsg)
             return
         else:
-            self.pubObserver.publish(False)
+            boolMsg.data = False
+            self.pubObserver.publish(boolMsg)
 
         x = self.pid_x.get_next_command(main_face_x)
         y = self.pid_y.get_next_command(main_face_y)
@@ -71,12 +72,12 @@ class FaceTracking:
         poseStamped.pose.position.x = x
         poseStamped.pose.position.y = y
 
-        rospy.loginfo("sent pose")
+        #rospy.loginfo("sent pose")
 
         self.pub.publish(poseStamped)
 
     def _distance_from_img_center(self, x, y):
-        return math.sqrt((self.img_center_x - x)**2 + (self.img_center_y)**2)
+        return math.sqrt((self.img_center_x - x)**2 + (self.img_center_y - y)**2)
 
     def _get_face_center_position(self, face):
         x = face.x + (face.width // 2)
