a7327edf9bf56a0b7c5e8c70ebde9099ea0b2377
Nicolas GB <guen3301@usherbrooke.ca>
2021-04-15 11:35:37 -0500

    version on my computer

--- homodeus_motivations/scenarios/states/state_09.py
+++ homodeus_motivations/scenarios/states/state_09.py
@@ -9,6 +9,7 @@ class State09(StateBase):
         self.order = ""
         # TODO set topic to correct name
         rospy.Subscriber("/obs_client_order", String, self.order_cb, queue_size=5)
+        self.talked = False
 
     def order_cb(self, order_string):
         self.order = "{TtsText: 'A client ordered: " + order_string.data + "'}"
@@ -25,13 +26,15 @@ class State09(StateBase):
         for desire in self.stateDict:
 
             if desire == "track_cook_09":
-                if self.stateDict[desire] == Event.ACC_ON:
+                if self.stateDict[desire] == Event.ACC_ON and not self.talked:
                     # Keep tracking as we continue sequence: no remove here
                     self.add("inform_cook_09", "Talking",  params = self.order)
                     self.stateDict["inform_cook_09"] = Event.DES_ON
+                    self.talked = True
                     return None
 
             if desire == "inform_cook_09": 
+                print("saw event on iform cook")
                 if self.stateDict[desire] == Event.ACC_ON:
                     self.remove("inform_cook_09")
                     self.stateDict.pop("inform_cook_09")
@@ -50,3 +53,4 @@ class State09(StateBase):
     def cleanup(self):
         self.remove("track_cook_09")
         self.stateDict.pop("track_cook_09")
+        self.talked = False
