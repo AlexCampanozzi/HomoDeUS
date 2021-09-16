7fbcdc36074ab3198e8d3cc48e5e27d509b287cb
kate <email>
2021-09-16 16:34:38 +0000

    created empty callback

---
+++ homodeus_behaviors/scripts/bhvr_pick_up.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+import rospy
+
+
+
+
+
+
+
+
+class bhvr_pick_up():
+    def __init__(self):
+        rospy.loginfo("initiating pick_up")
+
+    def wave():
+
+
+
+    def pick_up_callback(self, data):
+
+
+        
+    
+
+
+
+
