584b67dff49d7f08de5ff0855536c06be91c4896
kate <email>
2021-09-30 16:27:35 +0000

    Linked arm-controler node to bhvr

--- homodeus_behaviors/scripts/bhvr_take_plate.py
+++ homodeus_behaviors/scripts/bhvr_take_plate.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python2
 import rospy
 
-
-
-
-
-
-
-
-class bhvr_arm_mover():
+class bhvr_take_plate():
     def __init__(self):
-        rospy.loginfo("initiating pick_up")
-        rospy.Subscriber('/Proc_Arm_Mover', ArmMoves, self._arm_mover_callback, queue_size=5)
+        rospy.loginfo("initiating take_plate")
+
+        self.input_bhvr_goal = rospy.Subscriber("/bhvr_input_goal_take_plate",data_class=int,callback=self._take_plate_callback,queue_size=10)
+        self.input_bhvr_result = rospy.Subscriber("/bhvr_input_res_arm_controler",data_class=Bool,callback=self._res_callback,queue_size=10)
 
 
-    def _arm_mover_callback(self, data):
+        self.output_take_plate = rospy.Publisher("/arm_controler", data_class=int, queue_size=10)
+        self.output_bhvr_result = rospy.Publisher("/bhvr_output_res_take_plate", Bool, queue_size=10)
+
+
+    def _take_plate_callback(int data): #replace data by the type of the msg
         #data should be the position and orientation of the end effector and the pick or drop action
+        self.output_take_plate.publish(True) #replace true by real data
         #move_hand to position(data)
         #pick() or drop()
         #position of transport()
 
-
-    def pick(): #close hand on object
-
-
-
-    def drop(): #open hand to release object 
+    def _res_callback(Bool res) : #transmit the result of the cpp node to the goals
+        self.output_bhvr_result.publish(res)
 
 
-
-    def wave(): #series of position for the waving act
 
 
 
