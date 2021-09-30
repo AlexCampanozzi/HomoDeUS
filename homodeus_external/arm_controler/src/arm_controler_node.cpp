3151f54c9cd1707e770e6625d50dfbe9085f23a1
kate <email>
2021-09-30 18:34:42 +0000

    Adjusted custom msg + mvp arm_controler node

--- homodeus_external/arm_controler/src/arm_controler_node.cpp
+++ homodeus_external/arm_controler/src/arm_controler_node.cpp
@@ -1,9 +1,41 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <homodeus_arm_interface/ArmInterface.h>
+#include <arm_interface/ArmInterface.h>
 #include <custom_msgs.msg/ArmData>
 
-void _arm_controler_callback(int data){ //replace data by the type of the msg
+void _arm_controler_callback(ArmData data){ 
+    
+    bool success = false;
+
+    if (data.type.compare("joint") == 0)
+    {
+        rospy.loginfo("arm_interface_node: will attempt to move the arm in joints space.");
+        success = arm.moveToJoint(data.a, data.b, data.c, data.d, data.e, data.f, data.g, data.h);
+    }
+
+    else if (data.type.compare("coords") == 0)
+    {
+        rospy.loginfo("arm_interface_node: will attempt to move the arm in cartesian space.");
+        success = arm.moveToCartesian(data.a, data.b, data.c, data.d, data.e, data.f);
+    }
+
+    else
+    {
+        ROS_ERROR("arm_interface_node: wrong control type! arm_interface_node only takes joint for joints space or coords for cartesian space!");
+    }
+    
+    if (success)
+        rospy.loginfo("arm_interface_node: succeeded!");
+
+    else
+        rospy.loginfo("arm_interface_node: failed!");
+
+    ros::waitForShutdown();
+    return 0;
+    
+    
+    
+    //replace data by the type of the msg
 
         //data should be the position and orientation of the end effector and the pick or drop action
         //move_hand to position(data)
@@ -13,16 +45,20 @@ void _arm_controler_callback(int data){ //replace data by the type of the msg
 
 int main(int argc, char **argv)
 {
-
+    std::string controlType;
     ros::init(argc, argv, "listener");
+    ArmInterface arm;
+
+    rospy.loginfo("arm_interface_node is now running!");
+
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
     ros::NodeHandle _node;
     //        rospy.Subscriber('/Proc_Arm_Mover', ArmMoves, self._arm_mover_callback, queue_size=5)
     ros::Subscriber _date_subscriber = _node.subscribe("arm_controler", 10, _arm_controler_callback);
 
     ros::Publisher _res_publisher = _node.advertise<Bool>("res_arm_controler",10);
-
-    ros::spin(); //spin only once if using a while spinOnce()
 
     return 0;
 }
