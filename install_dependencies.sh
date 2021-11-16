37ab5667d9f3a8d463e6349dbe5a5ef0ddd113fa
Your Name <you@example.com>
2021-11-16 17:06:46 +0000

    Added some audio dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -13,5 +13,13 @@ pip2 install gtts
 pip2 install pytictoc
 pip2 install pydub
 sudo apt-get install -y libttspico-utils
-pico2wave -l fr-FR -w test.wav "Test de parole"
 sudo apt install ffmpeg
+
+pip2 install SpeechRecognition
+sudo apt-get install libasound-dev
+sudo apt-get install portaudio19-dev
+sudo apt-get install libportaudio2
+sudo apt-get install libportaudiocpp0
+pip2 install PyAudio
+sudo apt-get install python-sphinxbase
+
