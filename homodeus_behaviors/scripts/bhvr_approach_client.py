f0c7cc8575ed9098c142e688f73e79c6ef34e93d
Charles <chamai@live.ca>
2021-11-22 15:55:26 -0500

    Restored approach to same state as master to avoid merge trouble

--- homodeus_behaviors/scripts/bhvr_approach_client.py
+++ homodeus_behaviors/scripts/bhvr_approach_client.py
@@ -9,7 +9,6 @@ from sensor_msgs.msg import CameraInfo
 class ApproachClient():
     def __init__(self):
         self.vel_publisher = rospy.Publisher("/mobile_base/cmd_vel", Twist, queue_size=5)
-<<<<<<< Updated upstream
         self.target_box_size = 80000 # actual number TBD
         self.tolerance = 500 # actual number TBD
         # We are looking to be around 1.6-2m from the client
@@ -116,99 +115,6 @@ class ApproachClient():
                     self.min_range = scan.ranges[i]
 
             # TODO: some algo to decide what is a person vs not
-=======
-        self.pub = rospy.Publisher('tiago_head_controller', PoseStamped, queue_size=5)
-
-
-
-        ## movement test
-
-        poseStamped = PoseStamped()
-
-        x = 0
-        y = 0
-        poseStamped.pose.position.x = x
-        poseStamped.pose.position.y = y
-        self.pub.publish(poseStamped)
-
-        #navigator.gotoLandmark("wall")
-        self.navigator.gotoLandmark("kitchenEntrance")
-        # navigator.goto(0, 0, 0)
-
-        # self.pubGoto.publish("table1")
-
-
-    def _face_callback(self, detections):
-        
-        if self.value == 2:
-            self.value = 2
-
-            if self.depth_image is not None:
-                # rospy.loginfo((detections.faces))
-                # rospy.loginfo((detections.faces[0].x))
-                # rospy.loginfo((detections.faces[0].y))
-                # rospy.loginfo((detections.faces[0].width))
-                # rospy.loginfo((detections.faces[0].height))
-                # rospy.loginfo(detections)
-
-                face_depth_view = self.depth_image[detections.faces[0].y: detections.faces[0].y + detections.faces[0].height, 
-                                                detections.faces[0].x: detections.faces[0].x + detections.faces[0].width]
-
-                face_dist = np.nanmin(face_depth_view)
-
-                rospy.loginfo(face_dist)
-
-                if np.nanmin(face_depth_view) > self.approach_dist:
-
-                    # considering that the face is centered in the optical frame
-                    face_point = np.array([0, 0, face_dist])
-                    dist_to_approach = face_dist - self.approach_dist
-                    approach_point = np.array([0, 0, dist_to_approach])
-
-                    # rospy.loginfo(tf_listener.canTransform("/map", "/xtion_rgb_optical_frame", rospy.Duration(4.0)))
-
-                    # if tf_listener.canTransform("/map", "/xtion_rgb_optical_frame",rospy.Duration(4.0)):
-
-                    # tf_listener.waitForTransform("/map", "/xtion_rgb_optical_frame", rospy.Time(0), rospy.Duration(4.0))
-                    # essayer avec null et le timestamp du message
-
-                    point = PointStamped()
-                    point.point.x = approach_point[0]
-                    point.point.y = approach_point[1]
-                    point.point.z = approach_point[2]
-                    point.header.stamp = rospy.Time(0)
-                    point.header.frame_id = "/xtion_rgb_optical_frame"
-
-                    map_point = self.tfListener.transformPoint("/map", point)
-                    # self.navigator.goto(map_point[0], map_point[1], map_point[2])
-
-                    # rospy.loginfo(trans)
-                    # rospy.loginfo(rot)
-                    # rospy.loginfo(tranformation_matrix)
-                    rospy.loginfo(approach_point)
-                    rospy.loginfo(map_point)
-
-
-
-
-                    
-
-                # dist_
-                # rospy.loginfo(np.nanmean(face_depth_view))
-
-
-    def _camera_callback(self, image):
-
-        self.depth_image = self.bridge.imgmsg_to_cv2(image, "passthrough")
-        if self.value == 1:
-            self.value = 69
-            rospy.loginfo(self.depth_image.shape)
-            rospy.loginfo(np.max(self.depth_image))
-            rospy.loginfo(self.depth_image)
-
-        
-
->>>>>>> Stashed changes
 
 if __name__ == "__main__":
 
